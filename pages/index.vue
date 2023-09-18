5b67c79688bc5515be46eb591643a3cbc9739ee8
je <qwer90218@gmail.com>
2023-09-18 12:40:08 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1 class="bg-red-700">index</h1>
+    <p class=" text-emerald-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi harum amet iure necessitatibus aliquid tempore reprehenderit assumenda quia tenetur molestias explicabo voluptates, ipsam officia excepturi temporibus voluptate incidunt, esse doloribus!</p>
   </div>
 </template>
